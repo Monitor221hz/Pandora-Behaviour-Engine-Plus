08a07b361a36ffdd41e0c085d5759373a82d86e4
Screen <monitor.144.hz.100.srgb@gmail.com>
2023-08-28 02:42:41 +0100

    decoupled pack file edit methods

---
+++ PandoraPlus/Patch/Patchers/Skyrim/Hkx/PackFileEditor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using XmlCake.Linq;
+using XmlCake.String;
+
+namespace Pandora.Patch.Patchers.Skyrim.Hkx
+{
+	public partial class PackFileEditor
+	{
+		//these tuples are gross, pretend this implementation doesn't exist
+
+		private List<(string path, XElement element)> ReplaceEdits { get; set; } = new List<(string path, XElement element)>();
+
+		private List<(string path, XElement element)> InsertEdits { get; set; } = new List<(string path, XElement element)>();
+
+		private List<string> RemoveEdits { get; set; } = new List<string>(); 
+
+		private List<(string path, string oldValue, string newValue)> TextReplaceEdits { get; set; } = new List<(string path, string oldValue, string newValue)>();
+
+		private List<(string path, string insertValue)> TextInsertEdits { get; set; } = new List<(string path, string insertValue)>();
+
+		private List<(string path, string removeValue)> TextRemoveEdits { get; set; } = new List<(string path, string removeValue)>(); 
+
+
+
+
+
+
+		public void QueueReplaceElement(string path, XElement element) => ReplaceEdits.Add((path, element));
+
+		public void QueueInsertElement(string path, XElement element) => InsertEdits.Add((path, element));
+
+		public void QueueRemoveElement(string path) => RemoveEdits.Add(path);
+
+		public void QueueReplaceText(string path, string oldValue, string newValue) => TextReplaceEdits.Add((path, oldValue, newValue));
+
+		public void QueueInsertText(string path, string insertvalue) => TextInsertEdits.Add((path, insertvalue));
+
+		public void QueueRemoveText(string path, string removeValue) => TextRemoveEdits.Add((path, removeValue));
+
+
+
+		private void ApplyReplaceEdits(PackFile packFile)
+		{
+			foreach (var edit in ReplaceEdits)
+			{
+				ReplaceElement(packFile, edit.path, edit.element);
+			}
+		}
+
+		private void ApplyInsertEdits(PackFile packFile)
+		{
+			foreach (var edit in InsertEdits)
+			{
+				InsertElement(packFile, edit.path, edit.element);
+			}
+		}
+		private void ApplyTextReplaceEdits(PackFile packFile)
+		{
+			foreach (var edit in TextReplaceEdits)
+			{
+				ReplaceText(packFile, edit.path, edit.oldValue, edit.newValue);
+			}
+
+		}
+
+		private void ApplyTextInsertEdits(PackFile packFile)
+		{
+			foreach (var edit in TextInsertEdits)
+			{
+			}
+		}
+		public void ApplyEdits(PackFile packFile)
+		{
+			ApplyReplaceEdits(packFile);
+
+			ApplyInsertEdits(packFile); 
+
+			ApplyTextReplaceEdits(packFile);
+
+			ApplyTextInsertEdits(packFile);
+
+		}
+		
+
+	}
+}
---
+++ PandoraPlus/Patch/Patchers/Skyrim/Hkx/PackFileEdits.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml.Linq;
+using XmlCake.String;
+
+namespace Pandora.Patch.Patchers.Skyrim.Hkx
+{
+	public partial class PackFileEditor
+	{
+		public static void ReplaceElement(PackFile packFile, string path, XElement element) => packFile.Map.ReplaceElement(path, element);
+
+		public static void InsertElement(PackFile packFile, string path, XElement element)
+		{
+			packFile.Map.AppendElement(path.Substring(0, path.LastIndexOf('/')), element); 
+		}
+
+
+		public static void ReplaceText(PackFile packFile, string path, string oldValue, string newValue)
+		{
+			char[] trimChars = new char[3] { '\n', '\r', '\t' };
+			XElement element = packFile.Map.NavigateTo(path);
+			if (String.IsNullOrWhiteSpace(oldValue)) return;
+			string source = element.Value;
+			if (String.IsNullOrWhiteSpace(newValue))
+			{
+
+				int index = source.IndexOf(oldValue, StringComparison.Ordinal);
+				newValue = (index < 0) ? source : source.Remove(index, oldValue.Length);
+				element.SetValue(newValue);
+				return;
+			}
+			oldValue = oldValue.Trim().Replace("\t", "").Replace('\n', ' ');
+			newValue = newValue.Trim().Replace("\t", "").Replace('\n', ' ');
+
+			element.SetValue(source.Replace(oldValue, newValue, true));
+
+		}
+	}
+}
