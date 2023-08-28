05195d08a7714531688cc314a86ca778fe0dedac
Screen <monitor.144.hz.100.srgb@gmail.com>
2023-08-28 02:41:16 +0100

    incomplete debug exporter/importer

---
+++ PandoraPlus/Patch/IOManagers/Skyrim/XmlIOManager.cs
@@ -0,0 +1,23 @@
+﻿using Pandora.Core.IOManagers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandora.Patch.IOManagers.Skyrim
+{
+	public class XmlIOManager : IIOManager
+	{
+		public bool Export(FileInfo inFile, FileInfo outFile)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Import(FileInfo inFile)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
