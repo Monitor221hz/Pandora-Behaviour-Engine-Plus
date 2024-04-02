ca54261ca5289e1d832de63008f9bb2ab16be1aa
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2024-04-02 02:28:11 +0100

    chore: formatting

--- PandoraPlus/MVVM/Model/Patch/Patchers/Skyrim/SkyrimPatcher.cs
+++ PandoraPlus/MVVM/Model/Patch/Patchers/Skyrim/SkyrimPatcher.cs
@@ -25,128 +25,126 @@ public class SkyrimPatcher : IPatcher
 {
 	private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
-
 	private List<IModInfo> activeMods { get; set; } = new List<IModInfo>();
 
+	public void SetTarget(List<IModInfo> mods) => activeMods = mods;
+	private Exporter<PackFile> exporter = new PackFileExporter();
 
-    public void SetTarget(List<IModInfo> mods) => activeMods = mods;
-    private Exporter<PackFile> exporter = new PackFileExporter();
+	private NemesisAssembler nemesisAssembler { get; set; }
 
-    private NemesisAssembler nemesisAssembler { get; set; } 
-
-    private PandoraAssembler pandoraAssembler { get; set; }
+	private PandoraAssembler pandoraAssembler { get; set; }
 
 	public IPatcher.PatcherFlags Flags { get; private set; } = IPatcher.PatcherFlags.None;
 
 	private static readonly Version currentVersion = new Version(1, 3, 0);
 
-    private static readonly string versionLabel = "alpha";
+	private static readonly string versionLabel = "alpha";
 	public string GetVersionString() => $"{currentVersion.ToString()}-{versionLabel}";
 	public Version GetVersion() => currentVersion;
 
-    public SkyrimPatcher()
-    {
-        nemesisAssembler = new NemesisAssembler();
-		pandoraAssembler = new PandoraAssembler(nemesisAssembler);
-    }
-    public SkyrimPatcher(Exporter<PackFile> manager)
-    {
-        exporter = manager;
-        nemesisAssembler = new NemesisAssembler(manager);
+	public SkyrimPatcher()
+	{
+		nemesisAssembler = new NemesisAssembler();
 		pandoraAssembler = new PandoraAssembler(nemesisAssembler);
 	}
-    public string GetPostRunMessages()
-    {
-        StringBuilder logBuilder;
-        logBuilder = new StringBuilder("Resources loaded successfully.\r\n\r\n");
+	public SkyrimPatcher(Exporter<PackFile> manager)
+	{
+		exporter = manager;
+		nemesisAssembler = new NemesisAssembler(manager);
+		pandoraAssembler = new PandoraAssembler(nemesisAssembler);
+	}
+	public string GetPostRunMessages()
+	{
+		StringBuilder logBuilder;
+		logBuilder = new StringBuilder("Resources loaded successfully.\r\n\r\n");
 
 		for (int i = 0; i < activeMods.Count; i++)
-        {
+		{
 			IModInfo mod = activeMods[i];
-            string modLine = $"Pandora Mod {i + 1} : {mod.Name} - v.{mod.Version}";
+			string modLine = $"Pandora Mod {i + 1} : {mod.Name} - v.{mod.Version}";
 			logBuilder.AppendLine(modLine);
-            logger.Info(modLine);
-        }
+			logger.Info(modLine);
+		}
 
-        nemesisAssembler.GetPostMessages(logBuilder);
+		nemesisAssembler.GetPostMessages(logBuilder);
 
-        
-        return logBuilder.ToString();
-    }
 
-    public string GetFailureMessages()
-    {
+		return logBuilder.ToString();
+	}
+
+	public string GetFailureMessages()
+	{
 		StringBuilder logBuilder;
 		logBuilder = new StringBuilder("CRITICAL FAILURE \r\n\r\n");
 
 		if (Flags.HasFlag(PatcherFlags.UpdateFailed)) { logBuilder.AppendLine("Engine had one or more errors while updating."); }
 
-        logBuilder.Append("If the cause is unknown: submit a report to the author of the engine and attach Engine.log");
+		logBuilder.Append("If the cause is unknown: submit a report to the author of the engine and attach Engine.log");
 
 		return logBuilder.ToString();
 	}
 
-    public void Run()
-    {
-        //assembler.ApplyPatches();
-    }
-    public async Task<bool> RunAsync()
-    {
-        return await nemesisAssembler.ApplyPatchesAsync();
-    }
+	public void Run()
+	{
+		//assembler.ApplyPatches();
+	}
+	public async Task<bool> RunAsync()
+	{
+		return await nemesisAssembler.ApplyPatchesAsync();
+	}
 
-    public async Task<bool> UpdateAsync()
-    {
+	public async Task<bool> UpdateAsync()
+	{
 
-        logger.Info($"Skyrim Patcher {GetVersionString()}");
+		logger.Info($"Skyrim Patcher {GetVersionString()}");
 
 		//Parallel.ForEach(activeMods, mod => { assembler.AssemblePatch(mod); });
 
-        try
-        {
+		try
+		{
 			Parallel.ForEach(activeMods, mod =>
-            {
-                switch (mod.Format)
-                {
-                    case IModInfo.ModFormat.Nemesis:
-                        nemesisAssembler.AssemblePatch(mod);
-                        break;
-                    case IModInfo.ModFormat.Pandora:
-                        pandoraAssembler.AssemblePatch(mod);
-                        break;
-                    default:
-                        break;
-                }
+			{
+				switch (mod.Format)
+				{
+					case IModInfo.ModFormat.Nemesis:
+						nemesisAssembler.AssemblePatch(mod);
+						break;
+					case IModInfo.ModFormat.Pandora:
+						pandoraAssembler.AssemblePatch(mod);
+						break;
+					default:
+						break;
+				}
 			}
-            );
+			);
 		}
-        catch (Exception ex)
-        {
-            Flags |= PatcherFlags.UpdateFailed;
-            logger.Fatal($"Skyrim Patcher > Active Mods > Update > FAILED > {ex.ToString()}");
-        }
+		catch (Exception ex)
+		{
+			Flags |= PatcherFlags.UpdateFailed;
+			logger.Fatal($"Skyrim Patcher > Active Mods > Update > FAILED > {ex.ToString()}");
+		}
 
-        //await assembler.LoadResourcesAsync();
+		//await assembler.LoadResourcesAsync();
 
-        //List<Task> assembleTasks = new List<Task>();
-        //foreach (var mod in activeMods)
-        //{
-        //	assembleTasks.Add(Task.Run(() => { assembler.AssemblePatch(mod); }));
-        //}
-        //await Task.WhenAll(assembleTasks);
+		//List<Task> assembleTasks = new List<Task>();
+		//foreach (var mod in activeMods)
+		//{
+		//	assembleTasks.Add(Task.Run(() => { assembler.AssemblePatch(mod); }));
+		//}
+		//await Task.WhenAll(assembleTasks);
 
-        return !Flags.HasFlag(PatcherFlags.UpdateFailed);
+		return !Flags.HasFlag(PatcherFlags.UpdateFailed);
 	}
 
-    public async Task WriteAsync()
-    {
+	public async Task WriteAsync()
+	{
 
-    }
+	}
 
-    public void Update()
-    {
+	public void Update()
+	{
 
-    }
+	}
 
 	public async Task PreloadAsync()
 	{
