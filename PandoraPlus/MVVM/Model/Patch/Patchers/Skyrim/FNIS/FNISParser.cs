47e50cfa329686372d980e4ba90cb6ddf719dc44
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2024-04-02 23:28:42 +0100

    tentative fix: boars not animating

--- PandoraPlus/MVVM/Model/Patch/Patchers/Skyrim/FNIS/FNISParser.cs
+++ PandoraPlus/MVVM/Model/Patch/Patchers/Skyrim/FNIS/FNISParser.cs
@@ -70,7 +70,7 @@ public class FNISParser
 		{"chaurusflyer~chaurusflyerbehavior", "#0402" },
 		{"vampirebruteproject~vampirebrutebehavior", "#0502" },
 		{"benthiclurkerproject~benthiclurkerbehavior", "#0708" },
-		{"boarriekling~boarbehavior", "#0548" },
+		{"boarriekling~boarbehavior", "#0584" },
 		{"ballistacenturion~bcbehavior", "#0475" },
 		{"hmdaedra~hmdaedra", "#0490" },
 		{"netchproject~netchbehavior", "#0279" },
