cc3fc12a9a0378d49af0ebd7eb8718185a043000
Screen <monitor.144.hz.100.srgb@gmail.com>
2024-04-02 02:20:13 +0100

    version update 1.3.0

--- PandoraPlus/MVVM/Model/Patch/Patchers/Skyrim/SkyrimPatcher.cs
+++ PandoraPlus/MVVM/Model/Patch/Patchers/Skyrim/SkyrimPatcher.cs
@@ -38,7 +38,7 @@ public class SkyrimPatcher : IPatcher
 
 	public IPatcher.PatcherFlags Flags { get; private set; } = IPatcher.PatcherFlags.None;
 
-	private static readonly Version currentVersion = new Version(1, 2, 1);
+	private static readonly Version currentVersion = new Version(1, 3, 0);
 
     private static readonly string versionLabel = "alpha";
 	public string GetVersionString() => $"{currentVersion.ToString()}-{versionLabel}";
