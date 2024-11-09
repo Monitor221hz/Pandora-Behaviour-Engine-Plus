a650a11bb3ce842c76a86586af62c1e53ab109ca
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2024-11-09 15:26:02 +0000

    refactor: improve api params

--- Pandora API/Patch.Engine.Skyrim64/ISkyrim64Patch.cs
+++ Pandora API/Patch.Engine.Skyrim64/ISkyrim64Patch.cs
@@ -7,5 +7,6 @@ using System.Threading.Tasks;
 namespace Pandora.API.Patch.Engine.Skyrim64;
 public interface ISkyrim64Patch
 {
-	public void Run(IProjectManager projectManager);
+	public void SetOrigin(IModInfo origin);
+	public void Run(DirectoryInfo gameDirectory, IProjectManager projectManager);
 }
--- Pandora Behaviour Engine/ViewModels/EngineViewModel.cs
+++ Pandora Behaviour Engine/ViewModels/EngineViewModel.cs
@@ -99,7 +99,7 @@ namespace Pandora.ViewModels
         private bool modInfoCache = false;
         private static readonly DirectoryInfo workingDirectory = new DirectoryInfo(Directory.GetCurrentDirectory());
 
-        private DirectoryInfo currentDirectory = BehaviourEngine.SkyrimGameDirectory ?? workingDirectory;
+        private DirectoryInfo currentDirectory = BehaviourEngine.SkyrimGameDataDirectory ?? workingDirectory;
 
         private Task preloadTask;
 
