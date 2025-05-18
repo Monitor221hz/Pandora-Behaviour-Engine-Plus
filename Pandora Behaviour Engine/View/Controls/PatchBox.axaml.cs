ef423118d71e1dd020fdd61c535e2584da521ef2
dem0nd <artem1029111@gmail.com>
2025-05-18 19:27:48 +0400

    enchancement: added the ability to control the visibility of columns

--- Pandora Behaviour Engine/View/Controls/PatchBox.axaml.cs
+++ Pandora Behaviour Engine/View/Controls/PatchBox.axaml.cs
@@ -4,6 +4,7 @@ using Avalonia.LogicalTree;
 using Avalonia.ReactiveUI;
 using Avalonia.VisualTree;
 using Pandora.ViewModels;
+using System;
 
 namespace Pandora.Views;
 
@@ -21,15 +22,34 @@ public partial class PatchBox : ReactiveUserControl<EngineViewModel>
 		var savedColumn1 = Properties.GUISettings.Default.PatchBoxNameColumnWidth;
 		var savedColumn2 = Properties.GUISettings.Default.PatchBoxAuthorColumnWidth;
 		var savedColumn3 = Properties.GUISettings.Default.PatchBoxVersionColumnWidth;
-		ModInfoDataGrid.Columns[1].Width = savedColumn1 > 1 ? new DataGridLength(savedColumn1) : ModInfoDataGrid.Columns[1].Width;
+        var savedColumn4 = Properties.GUISettings.Default.PatchBoxPriorityColumnWidth;
+        var savedColumn5 = Properties.GUISettings.Default.PatchBoxCodeColumnWidth;
+
+        ModInfoDataGrid.Columns[1].Width = savedColumn1 > 1 ? new DataGridLength(savedColumn1) : ModInfoDataGrid.Columns[1].Width;
 		ModInfoDataGrid.Columns[2].Width = savedColumn2 > 1 ? new DataGridLength(savedColumn2) : ModInfoDataGrid.Columns[2].Width;
 		ModInfoDataGrid.Columns[3].Width = savedColumn3 > 1 ? new DataGridLength(savedColumn3) : ModInfoDataGrid.Columns[3].Width;
-	}
+        ModInfoDataGrid.Columns[4].Width = savedColumn4 > 1 ? new DataGridLength(savedColumn4) : ModInfoDataGrid.Columns[4].Width;
+        ModInfoDataGrid.Columns[5].Width = savedColumn5 > 1 ? new DataGridLength(savedColumn5) : ModInfoDataGrid.Columns[5].Width;
+
+        ModInfoDataGrid.Columns[1].IsVisible = Properties.GUISettings.Default.PatchBoxNameColumnVisible;
+        ModInfoDataGrid.Columns[2].IsVisible = Properties.GUISettings.Default.PatchBoxAuthorColumnVisible;
+        ModInfoDataGrid.Columns[3].IsVisible = Properties.GUISettings.Default.PatchBoxVersionColumnVisible;
+        ModInfoDataGrid.Columns[4].IsVisible = Properties.GUISettings.Default.PatchBoxPriorityColumnVisible;
+        ModInfoDataGrid.Columns[5].IsVisible = Properties.GUISettings.Default.PatchBoxCodeColumnVisible;
+    }
 
 	private void ModInfoDataGrid_DetachedFromLogicalTree(object? sender, LogicalTreeAttachmentEventArgs e)
 	{
 		Properties.GUISettings.Default.PatchBoxNameColumnWidth = ModInfoDataGrid.Columns[1].Width.Value;
 		Properties.GUISettings.Default.PatchBoxAuthorColumnWidth = ModInfoDataGrid.Columns[2].Width.Value;
 		Properties.GUISettings.Default.PatchBoxVersionColumnWidth = ModInfoDataGrid.Columns[3].Width.Value;
-	}
+        Properties.GUISettings.Default.PatchBoxPriorityColumnWidth = ModInfoDataGrid.Columns[4].Width.Value;
+        Properties.GUISettings.Default.PatchBoxCodeColumnWidth = ModInfoDataGrid.Columns[5].Width.Value;
+
+        Properties.GUISettings.Default.PatchBoxNameColumnVisible = ModInfoDataGrid.Columns[1].IsVisible;
+        Properties.GUISettings.Default.PatchBoxAuthorColumnVisible = ModInfoDataGrid.Columns[2].IsVisible;
+        Properties.GUISettings.Default.PatchBoxVersionColumnVisible = ModInfoDataGrid.Columns[3].IsVisible;
+        Properties.GUISettings.Default.PatchBoxPriorityColumnVisible = ModInfoDataGrid.Columns[4].IsVisible;
+        Properties.GUISettings.Default.PatchBoxCodeColumnVisible = ModInfoDataGrid.Columns[5].IsVisible;
+    }
 }
