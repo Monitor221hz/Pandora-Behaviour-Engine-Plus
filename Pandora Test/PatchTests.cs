21885703c5307f4edf7056662b2a57ecdf1d1ed2
Monitor144hz <monitor.144.hz.100.srgb@gmail.com>
2025-05-22 13:59:42 +0100

    feat: basic unit testing

---
+++ Pandora Test/PatchTests.cs
@@ -0,0 +1,20 @@
+﻿using Pandora.Models;
+using Pandora.Services;
+
+namespace PandoraTests;
+
+public class PatchTests
+{
+	[Fact]
+	public async void NemesisTest()
+	{
+		ModService service = new ModService(Path.Combine(BehaviourEngine.AssemblyDirectory.FullName, "Pandora_Engine", "ActiveMods.txt"));
+		var mods = await service.LoadModsAsync(BehaviourEngine.AssemblyDirectory, new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "Data")));
+		BehaviourEngine engine = new BehaviourEngine();
+		DirectoryInfo outputDirectory = new DirectoryInfo(Path.Combine(Directory.GetCurrentDirectory(), "Output"));
+		outputDirectory.Create();
+		engine.SetOutputPath(outputDirectory);
+		await engine.PreloadAsync();
+		await engine.LaunchAsync(mods);
+	}
+}
