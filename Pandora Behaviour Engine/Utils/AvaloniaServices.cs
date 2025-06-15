fd262a8106b86084ec73d79c0a0a54b97da46ffe
dem0nd <artem1029111@gmail.com>
2025-06-15 18:46:10 +0400

    refactor: static function utils

---
+++ Pandora Behaviour Engine/Utils/AvaloniaServices.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using System;
+using System.Threading.Tasks;
+
+namespace Pandora.Utils;
+
+public static class AvaloniaServices
+{
+	public static async Task DoSetClipboardTextAsync(string? text)
+	{
+		if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
+			desktop.MainWindow?.Clipboard is not { } provider)
+			throw new NullReferenceException("Missing Clipboard instance.");
+
+		await provider.SetTextAsync(text);
+	}
+
+	public static async Task DoLaunchUriAsync(string uri)
+	{
+		if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
+			desktop.MainWindow?.Launcher is not { } provider)
+			throw new NullReferenceException("Missing Launcher instance.");
+
+		await provider.LaunchUriAsync(new Uri(uri));
+	}
+}
---
+++ Pandora Behaviour Engine/Utils/EngineLogger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reactive.Subjects;
+using System.Text;
+
+namespace Pandora.Utils;
+
+public static class EngineLogger
+{
+	private static readonly StringBuilder _logBuilder = new();
+	private static readonly Subject<string> _logSubject = new();
+
+	public static IObservable<string> LogObservable => _logSubject;
+
+	public static void AppendLine(string message)
+	{
+		_logBuilder.AppendLine(message);
+		_logSubject.OnNext(_logBuilder.ToString());
+	}
+
+	public static void Clear()
+	{
+		_logBuilder.Clear();
+		_logSubject.OnNext(string.Empty);
+	}
+}
