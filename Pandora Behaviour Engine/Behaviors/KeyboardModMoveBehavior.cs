5d3cc8829541619774e003dd6cef5c0e765804ae
dem0nd <artem1029111@gmail.com>
2025-06-15 18:15:50 +0400

    feature: keyboard based sorting - improve logic sorting

---
+++ Pandora Behaviour Engine/Behaviors/KeyboardModMoveBehavior.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+using Pandora.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandora.Behaviors;
+
+public class KeyboardModMoveBehavior : StyledElementBehavior<DataGrid>
+{
+	public ModsDataGridDropHandler? DropHandler { get; set; }
+
+	protected override void OnAttached()
+	{
+		base.OnAttached();
+		AssociatedObject!.KeyDown += OnKeyDown;
+	}
+
+	protected override void OnDetaching()
+	{
+		base.OnDetaching();
+		AssociatedObject!.KeyDown -= OnKeyDown;
+	}
+
+	private void OnKeyDown(object? sender, KeyEventArgs e)
+	{
+		if (AssociatedObject.DataContext is not EngineViewModel vm || DropHandler is null)
+			return;
+
+		bool isCtrl = (e.KeyModifiers & KeyModifiers.Control) == KeyModifiers.Control;
+
+		if (e.Key == Key.Add || e.Key == Key.OemPlus || (isCtrl && e.Key == Key.Down))
+		{
+			DropHandler.MoveDown(AssociatedObject, vm);
+			e.Handled = true;
+		}
+		else if (e.Key == Key.Subtract || e.Key == Key.OemMinus || (isCtrl && e.Key == Key.Up))
+		{
+			DropHandler.MoveUp(AssociatedObject, vm);
+			e.Handled = true;
+		}
+	}
+}
