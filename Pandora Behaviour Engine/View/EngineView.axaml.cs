2221f47a969e0b104b1bdcd7563b3d68ad65ffe3
dem0nd <artem1029111@gmail.com>
2025-07-23 21:24:44 +0300

    refactor: binding EngineViewModel via DataContext in XAML

--- Pandora Behaviour Engine/View/EngineView.axaml.cs
+++ Pandora Behaviour Engine/View/EngineView.axaml.cs
@@ -1,16 +1,11 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using Pandora.ViewModels;
 
 namespace Pandora.Views;
 
 public partial class EngineView : UserControl
 {
-	private EngineViewModel _viewModel;
 	public EngineView()
     {
         InitializeComponent();
-		_viewModel = new EngineViewModel();
-		DataContext = _viewModel;
 	}
 }
