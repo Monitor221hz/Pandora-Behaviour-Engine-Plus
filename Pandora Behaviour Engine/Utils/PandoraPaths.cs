5148987e33080274ece657c4c211e2b4f86885b3
dem0nd <artem1029111@gmail.com>
2025-08-08 23:17:43 +0300

    refactor: moving the path to ActiveMods.json to a separate class, when accessing which Pandora_Engine folder is lazily created

---
+++ Pandora Behaviour Engine/Utils/PandoraPaths.cs
@@ -0,0 +1,19 @@
+﻿using Pandora.Logging;
+using System;
+using System.IO;
+
+namespace Pandora.Utils;
+
+public static class PandoraPaths
+{
+	public static DirectoryInfo Root => _root.Value;
+	private static readonly Lazy<DirectoryInfo> _root = new(() =>
+	{
+		var outputDir = LaunchOptions.Current.OutputDirectory?.FullName ?? Environment.CurrentDirectory;
+		var dir = new DirectoryInfo(Path.Combine(outputDir, "Pandora_Engine"));
+		dir.Create();
+		return dir;
+	});
+
+	public static FileInfo ActiveModsFile => new(Path.Combine(Root.FullName, "ActiveMods.json"));
+}
