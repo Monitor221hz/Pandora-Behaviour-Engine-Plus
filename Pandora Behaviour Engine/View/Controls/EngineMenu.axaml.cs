1b882492d6e1186ee7ab17d2026de08aef528493
dem0nd <artem1029111@gmail.com>
2025-08-08 22:09:38 +0300

    refactor: create dialog in xaml code

--- Pandora Behaviour Engine/View/Controls/EngineMenu.axaml.cs
+++ Pandora Behaviour Engine/View/Controls/EngineMenu.axaml.cs
@@ -23,52 +23,7 @@ public partial class EngineMenu : ReactiveUserControl<EngineViewModel>
                 vm => vm.ShowAboutDialog,
                 async context =>
                 {
-                    var dialogViewModel = context.Input;
-                    var aboutDialog = new TaskDialog
-                    {
-                        DataContext = dialogViewModel,
-                        Header = AboutDialogViewModel.Header,
-                        SubHeader = AboutDialogViewModel.SubHeader,
-                        Content = AboutDialogViewModel.Content,
-                        FooterVisibility = TaskDialogFooterVisibility.Never,
-                        IsFooterExpanded = false,
-                        IconSource = (IconSource)Application.Current.FindResource("IconPandora"),
-                        ShowProgressBar = false
-                    };
-                    aboutDialog.Commands.Add(new TaskDialogCommand
-                    {
-                        Text = "Check Update",
-                        Description = "Check for a new version",
-                        IsEnabled = false,
-                        ClosesOnInvoked = false,
-                        IconSource = new SymbolIconSource { Symbol = Symbol.Refresh }
-                    });
-                    aboutDialog.Commands.Add(new TaskDialogCommand
-                    {
-                        Text = "GitHub",
-                        Description = "Visit GitHub page",
-                        IsEnabled = true,
-                        Command = ViewModel!.OpenUrlCommand,
-                        CommandParameter = "https://github.com/Monitor221hz/Pandora-Behaviour-Engine-Plus",
-                        ClosesOnInvoked = false,
-                        IconSource = (IconSource)Application.Current.FindResource("IconGithub")
-                    });
-                    aboutDialog.Commands.Add(new TaskDialogCommand
-                    {
-                        Text = "Discord",
-                        Description = "Join the Discord group",
-                        IsEnabled = true,
-                        Command = ViewModel!.OpenUrlCommand,
-                        CommandParameter = "https://discord.gg/8nUQCWMn3w",
-                        ClosesOnInvoked = false,
-                        IconSource = (IconSource)Application.Current.FindResource("IconDiscord")
-                    });
-
-                    aboutDialog.Buttons.Add(new TaskDialogButton("Close", TaskDialogStandardResult.Close));
-
-                    aboutDialog.XamlRoot = (Visual)VisualRoot;
-
-                    await aboutDialog.ShowAsync(true);
+					await AboutDialog.ShowAsync(true);
                     context.SetOutput(Unit.Default);
                 });
         });
