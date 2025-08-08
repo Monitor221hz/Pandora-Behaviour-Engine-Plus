ab3cf8204ae333de22c7b7c50ce88e59d3a278bd
dem0nd <artem1029111@gmail.com>
2025-08-08 22:30:56 +0300

    refactor: remove Debug logging

--- Pandora Behaviour Engine/Utils/ViewModelSettingsHelper.cs
+++ Pandora Behaviour Engine/Utils/ViewModelSettingsHelper.cs
@@ -17,7 +17,6 @@ public static class ViewModelSettingsHelper
 		{
 			assignToSetting(value);
 			Properties.GUISettings.Default.Save();
-			Debug.WriteLine($"[Save] {assignToSetting.Method.Name}: {value}");
 		});
 	}
 }
