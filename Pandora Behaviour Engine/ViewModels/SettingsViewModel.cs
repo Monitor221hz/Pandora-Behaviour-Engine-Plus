62b23a086e390775ce25a6bc8df89ba620477e79
dem0nd <artem1029111@gmail.com>
2025-08-08 22:21:14 +0300

    refactor: change theme by subscription change value via toggle

---
+++ Pandora Behaviour Engine/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Platform;
+using Avalonia.Styling;
+using Pandora.Utils;
+using ReactiveUI;
+using ReactiveUI.SourceGenerators;
+
+namespace Pandora.ViewModels;
+
+public partial class SettingsViewModel : ViewModelBase
+{
+	[Reactive] private bool _themeToggleState = Properties.GUISettings.Default.AppTheme == (int)PlatformThemeVariant.Dark;
+
+	public SettingsViewModel()
+	{
+		ViewModelSettingsHelper.BindSetting(
+			this.WhenAnyValue(vm => vm.ThemeToggleState),
+			isDark =>
+			{
+				Application.Current!.RequestedThemeVariant = isDark
+					? ThemeVariant.Dark
+					: ThemeVariant.Light;
+
+				Properties.GUISettings.Default.AppTheme = isDark ? 1 : 0;
+				Properties.GUISettings.Default.Save();
+			}
+		);
+	}
+}
