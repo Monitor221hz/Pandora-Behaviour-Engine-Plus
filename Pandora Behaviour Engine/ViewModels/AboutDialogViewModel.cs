caddf95842cba069f48dc2808aaf64593b6c73b1
dem0nd <artem1029111@gmail.com>
2025-08-17 16:00:01 +0300

    refactor: centralized logic for obtaining the current version

--- Pandora Behaviour Engine/ViewModels/AboutDialogViewModel.cs
+++ Pandora Behaviour Engine/ViewModels/AboutDialogViewModel.cs
@@ -1,13 +1,13 @@
-﻿using System.Reflection;
+﻿using Pandora.Utils;
+using System.Reflection;
 
 namespace Pandora.ViewModels;
 
 public class AboutDialogViewModel : ViewModelBase
 {
-    public static string Version => Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion.Split('+')[0] ?? "Unknown";
-    public static string Header => Assembly.GetExecutingAssembly().GetName().Name ?? "Unknown";
-    public static string SubHeader => $"Version: {Version}";
-    public static string Content => "Behaviour engine tool for patching Skyrim Nemesis/FNIS behaviour mods, with full creature support.";
+	public static string SubHeader => $"Version: {AppInfo.Version}";
+	public static string Header => AppInfo.Name;
+	public static string Content => "Behaviour engine tool for patching Skyrim Nemesis/FNIS behaviour mods, with full creature support.";
 
     public AboutDialogViewModel() { }
 
