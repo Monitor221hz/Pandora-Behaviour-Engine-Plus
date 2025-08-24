51d7a74e884e2dddbefe6c7e786b6817e740bbe5
dem0nd <artem1029111@gmail.com>
2025-08-24 11:44:48 +0300

    chore: add license header

--- Pandora Tests/SkyrimPathResolverPriorityTests.cs
+++ Pandora Tests/SkyrimPathResolverPriorityTests.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Win32;
+﻿// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2023-2025 Pandora Behaviour Engine Contributors
+
+using Microsoft.Win32;
 using Pandora.Utils;
 
 namespace PandoraTests;
