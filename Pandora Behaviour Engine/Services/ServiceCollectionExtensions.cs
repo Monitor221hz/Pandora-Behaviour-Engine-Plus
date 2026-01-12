519de7ece3258ea77adc288fda93452a624b96f6
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2026-01-12 18:16:40 +0100

    [chore]: Update copyright year to 2026

--- Pandora Behaviour Engine/Services/ServiceCollectionExtensions.cs
+++ Pandora Behaviour Engine/Services/ServiceCollectionExtensions.cs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2023-2025 Pandora Behaviour Engine Contributors
+// Copyright (C) 2023-2026 Pandora Behaviour Engine Contributors
 
 using System;
 using GameFinder.Common;
