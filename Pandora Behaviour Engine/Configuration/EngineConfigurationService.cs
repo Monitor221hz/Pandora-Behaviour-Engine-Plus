8f5413c05720966e1ab3a6a128699a9aec522e7b
dem0nd <artem1029111@gmail.com>
2026-01-23 20:37:21 +0300

    fix: remove old license header

--- Pandora Behaviour Engine/Configuration/EngineConfigurationService.cs
+++ Pandora Behaviour Engine/Configuration/EngineConfigurationService.cs
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2023-2026 Pandora Behaviour Engine Contributors
 
-﻿// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2023-2025 Pandora Behaviour Engine Contributors
-
 using Pandora.API.Patch.Config;
 using Pandora.Models.Patch.Configs;
 using System;
--- Pandora Behaviour Engine/View/EngineViewModel.cs
+++ Pandora Behaviour Engine/View/EngineViewModel.cs
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Copyright (C) 2023-2026 Pandora Behaviour Engine Contributors
 
-﻿// SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright (C) 2023-2025 Pandora Behaviour Engine Contributors
-
 namespace Pandora.ViewModels;
 
 public partial class EngineViewModel : ViewModelBase
