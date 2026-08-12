d4a48099d17059f20f978df826f64c0abff641da
Monitor221hz <monitor.144.hz.100.srgb@gmail.com>
2026-08-12 22:59:54 +0100

    fix: debug argument

--- Pandora.Skyrim/Services.cs
+++ Pandora.Skyrim/Services.cs
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier-Identifier: GPL-3.0-or-later
 // Copyright (C) 2023-2026 Pandora Behaviour Engine Contributors
 
+using System;
 using Microsoft.Extensions.DependencyInjection;
 using Pandora.API.Patch.IOManagers;
 using Pandora.API.Patch.Skyrim64;
@@ -8,11 +9,11 @@ using Pandora.API.Patch.Skyrim64.AnimData;
 using Pandora.API.Patch.Skyrim64.AnimSetData;
 using Pandora.Skyrim.AnimData;
 using Pandora.Skyrim.AnimSetData;
+using Pandora.Skyrim.CLI;
 using Pandora.Skyrim.Format.FNIS;
 using Pandora.Skyrim.Format.Nemesis;
 using Pandora.Skyrim.Format.Pandora;
 using Pandora.Skyrim.Patch.IO;
-using System;
 
 namespace Pandora.Skyrim;
 
@@ -23,11 +24,21 @@ public static class Services
 		public IServiceCollection AddSkyrimPatchServices()
 		{
 			return serviceCollection
+				.AddSingleton<DebugPackFileExporter>()
+				.AddSingleton<PackFileExporter>()
+				.AddSingleton<IMetaDataExporter<IPackFile>>(sp =>
+				{
+					var options = sp.GetService<LaunchOptions>();
 #if DEBUG
-				.AddSingleton<IMetaDataExporter<IPackFile>, DebugPackFileExporter>()
+					return sp.GetRequiredService<DebugPackFileExporter>();
 #else
-				.AddSingleton<IMetaDataExporter<IPackFile>, PackFileExporter>()
+					if (options is { UseSkyrimDebug64: true })
+					{
+						return sp.GetRequiredService<DebugPackFileExporter>();
+					}
+					return sp.GetRequiredService<PackFileExporter>();
 #endif
+				})
 				.AddScoped<IFNISParser, FNISParser>()
 				.AddScoped<IProjectManager, ProjectManager>()
 				.AddScoped<IAnimDataManager, AnimDataManager>()
